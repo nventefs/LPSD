a1d244e87824c2ef95829f0d367f3b7cda936a45
Greg Martinjak <greg.martinjak@nvent.com>
2022-07-27 20:39:23 -0400

    added csv output code

--- checkmultiplicative.py
+++ checkmultiplicative.py
@@ -5,12 +5,15 @@ from types import NoneType
 import numpy as np
 import json
 
-rows        = []
-row         = []
-jsonnames   = ['Cvm_Test_Case_1 Metric_V61_7-7-22', 'Cvm_Test_Case_2_V47_7-8-22', 'Cvm_Test_Case_3_RevB_V26_7-8-22',\
-                'Cvm_Test_Case_4_V41_7-22-22',  'Cvm_Test_Case_5_V52_7-8-22', '', 'Cvm_Test_Case_7_V55_7-8-22']
-
 # IMPORTANT: prior to running checkmultiplicative, run checkreductive and add GUID values into CSV file
+
+levels              = []
+level_z             = []
+level_g             = []
+rows                = []
+row                 = []
+jsonnames           = ['Cvm_Test_Case_1 Metric_V61_7-7-22', 'Cvm_Test_Case_2_V47_7-8-22', 'Cvm_Test_Case_3_RevB_V26_7-8-22',\
+                        'Cvm_Test_Case_4_V41_7-22-22',  'Cvm_Test_Case_5_V52_7-8-22', '', 'Cvm_Test_Case_7_V55_7-8-22']
 
 def min_width(level_guid): 
     x = []
@@ -45,6 +48,11 @@ def csv_read(datafile): # import csv file
             rows.append(row)
         return rows
 
+def csv_write(row): # write the csv outfile
+    with open('outfile.csv', 'a', newline='') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        spamwriter.writerow(row)
+
 def row_count(csv_file): # count total rows in csv file
     with open(csv_file, 'r', newline='') as csvfile:
         row_count = sum(1 for row in csvfile)
@@ -62,38 +70,10 @@ checkpoints = csv_read(csv_name)
 f = open(json_name)
 data = json.load(f)
 
-levels              = []
-level_z             = []
-level_g             = []
-level_min_width     = []
-x                   = []
-y                   = []
-
 for i in data['levels']:
     levels.append(i['levelName'])
     level_z.append(i['elevation'])
     level_g.append(i['levelGuid'])
-"""
-for k in level_g:
-    print(k)
-    #level_min_width.append(min_width(k))
-    print(min_width(k)) 
-"""
-#min_width(level_g[0])
-#print(level_g[0])
-#print(levels)
-#print(level_g)
-#print(level_min_width)
-
-
-#print(levels)
-#print(level_z)
-#print(level_g)
-#print(min_width("6fc52480-a9fb-4be7-9c1f-d8bb797b0c5e")) 
-#for k in range(1,row_count(csv_name)): 
-#    print(checkpoints[k][11])
-
-
 
 for k in range(1,row_count(csv_name)):   
     for i in data['points']:
@@ -105,73 +85,77 @@ for k in range(1,row_count(csv_name)):
             if(i['levelGuid'] != level_g[len(level_g)-1] and not(i['extendedPoint'])):
                 #Equation 3
                 if(i['isCorner']): # Equation A
-                    #print("A")
                     H = i['position']['z']-level_z[len(level_z)-1]
                     W = min_width(i['levelGuid'])
-                    multi_3 = multiplicative.eq_a(H, W, 0.38)
+                    multi_3 = [multiplicative.eq_a(H, W, 0.38), "A"]
                 elif(i['isEdgeRectangular']): # Equation B
-                    print("B")
                     H = i['position']['z']-level_z[len(level_z)-1]
                     W = min_width(i['levelGuid'])
-                    multi_3 = multiplicative.eq_b(H, W, 0.38)
+                    multi_3 = [multiplicative.eq_b(H, W, 0.38), "B"]
                 elif(i['isFaceHorizontal']): # Equation C
-                    print("C")
                     H = i['position']['z']
                     W = min_width(i['levelGuid'])
-                    multi_3 = multiplicative.eq_c(H, W, 0.38)
+                    multi_3 = [multiplicative.eq_c(H, W, 0.38), "C"]
                 elif(i['isEdgeOval']): # Equation D
-                    print("D")
                     H = i['position']['z']-level_z[len(level_z)-1]
                     W = min_width(i['levelGuid'])
-                    multi_3 = multiplicative.eq_b(H, W, 0.38)
+                    multi_3 = [multiplicative.eq_b(H, W, 0.38), "D"]
                 else: # Equation E
-                    print("E")
                     H = i['position']['z']
                     W = min_width(i['levelGuid'])
-                    multi_3 = multiplicative.eq_c(H, W, 0.38)
+                    multi_3 = [multiplicative.eq_c(H, W, 0.38), "E"]
 
                 #Equation 4
                 if(i['isCorner']):
-                    print("Q")
-                    multi_4 = multiplicative.eq_q(i['position']['z'])
+                    multi_4 = [multiplicative.eq_q(0.05), "Q"]
                 elif(i['isEdgeOval'] or i['isEdgeRectangular']):
-                    print("R")
-                    multi_4 = multiplicative.eq_r()
+                    multi_4 = [multiplicative.eq_r(), "R"]
                 else:
-                    print("S")
-                    multi_4 = multiplicative.eq_s(i['position']['z'])
+                    multi_4 = [multiplicative.eq_s(0.05), "S"]
                     
             #Equation 5
             if (i['levelGuid'] == level_g[len(level_g)-1] or i['extendedPoint']):
                 if(i['isCorner']):
-                    print("A")
                     H = i['position']['z']
                     W = min_width(i['levelGuid'])
-                    multi_5 = multiplicative.eq_a(H, W, 0.38)
+                    print(H)
+                    print(W)
+                    multi_5 = [multiplicative.eq_a(H, W, 0.38), "A"]
                 elif(i['isEdgeRectangular']):
-                    print("B")
                     H = i['position']['z']
                     W = min_width(i['levelGuid'])
-                    multi_5 = multiplicative.eq_b(H, W, 0.38)
+                    multi_5 = [multiplicative.eq_b(H, W, 0.38), "B"]
                 elif(i['isFaceHorizontal']):
-                    print("C")
                     H = i['position']['z']
                     W = min_width(i['levelGuid'])
-                    multi_5 = multiplicative.eq_c(H, W, 0.38)
+                    multi_5 = [multiplicative.eq_c(H, W, 0.38), "C"]
                 elif(i['isEdgeOval']):
-                    print("D")
                     H = i['position']['z']
                     W = min_width(i['levelGuid'])
-                    multi_5 = multiplicative.eq_d(H, W, 0.38)
+                    multi_5 = [multiplicative.eq_d(H, W, 0.38), "D"]
                 else:
-                    print("E")
                     H = i['position']['z']
                     W = min_width(i['levelGuid'])
-                    multi_5 = multiplicative.eq_e(H, W, 0.38)
+                    multi_5 = [multiplicative.eq_e(H, W, 0.38), "E"]
             else:
                 if(i['isCorner'] or i['isEdgeRectangular']):
-                    print("L") # figure this one out separately
+                    H = level_z[len(level_z)-1]
+                    W = min_width(level_g[len(level_g)-1])
+                    Hf = i['position']['z'] - H
+                    multi_5 = [multiplicative.eq_l(H, W, Hf), "L"]
                 else:
-                    print("N")
+                    H = i['position']['z']-level_z[len(level_z)-1]
+                    W = min_width(i['levelGuid'])
+                    Hf = i['position']['z'] - H
+                    multi_5 = [multiplicative.eq_n(H, W, Hf), "N"]
+
+            if(multi_3 == 1): multi_3 = [1, ""]
+            if(multi_4 == 1): multi_4 = [1, ""]
+            if(multi_5 == 1): multi_5 = [1, ""]
+            
+            row = [checkpoints[k][0], checkpoints[k][2], checkpoints[k][3], checkpoints[k][4], i['position']['x'], i['position']['y'], i['position']['z'], \
+                    checkpoints[k][12], checkpoints[k][10], i['kiTotalMultiplicative'], multi_3[0]*multi_4[0]*multi_5[0], multi_3[0], multi_3[1], \
+                    multi_4[0], multi_4[1], multi_5[0], multi_5[1]]
+            csv_write(row)
 
 f.close()
